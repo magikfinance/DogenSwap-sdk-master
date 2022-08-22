77560f739da9d77efcb6d63056c9afd26ed9be26
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-08-22 12:36:46 +0100

    Revert "update"
    This reverts commit e03e6d21327266729583a6ca9b5fe6909b6d27c1.

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'WWDOGE', 'Wrapped Doge')
+  public static readonly ETHER: Currency = new Currency(18, 'MATIC', 'Matic')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
